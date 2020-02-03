81af33bf412b9f90754a0585065b6617d4a94c2c
David <david.he07@yahoo.ca>
2020-02-03 14:47:11 -0500

    implemented function of assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed:", `✅ ✅ ✅  ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log("Assertion Failed:", `🛑 🛑 🛑  ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
