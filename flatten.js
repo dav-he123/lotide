7ff81b7a121f2e36b7f6790344d16275b02e5c47
David <david.he07@yahoo.ca>
2020-02-04 20:50:06 -0500

    flatten.js file completed

---
+++ flatten.js
@@ -0,0 +1,35 @@
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`Assertion Passed:, ✅ ✅ ✅  ${actual} === ${expected}`);
+  }
+
+  if (!eqArrays(actual, expected)) {
+    console.log(`Assertion Failed:, 🛑 🛑 🛑  ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const flatten = function(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        result.push(arr[i][j]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  console.log(result);
+  return result;
+};
+
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
