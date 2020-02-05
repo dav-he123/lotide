37ac245c0c90a7671f57cb9fb7773d6ed4eedd8d
David <david.he07@yahoo.ca>
2020-02-05 15:23:43 -0500

    completed findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:, ✅ ✅ ✅  ${actual} === ${expected}`);
+  }
+
+  if (actual !== expected) {
+    console.log(`Assertion Failed:, 🛑 🛑 🛑  ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(genre, title) {
+  console.log(genre);
+  for (let category of Object.keys(genre)) {
+    if (genre[category] === title) {
+      return category;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
