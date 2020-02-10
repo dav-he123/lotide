8b864d852071a7e056ced80c61a9dabc91d78461
David <david.he07@yahoo.ca>
2020-02-10 13:03:22 -0500

    completed head.js file

--- head.js
+++ head.js
@@ -1,16 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:, ✅ ✅ ✅  ${actual} === ${expected}`);
-  }
-
-  if (actual !== expected) {
-    console.log(`Assertion Failed:, 🛑 🛑 🛑  ${actual} !== ${expected}`);
-  }
-};
+// const assertEqual = require("./assertEqual");
 
 const head = function(actual) {
   return actual[0];
 };
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+//Test Code
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
