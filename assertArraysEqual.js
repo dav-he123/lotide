3b4b3df56ed732998708959580b528396bce793c
David <david.he07@yahoo.ca>
2020-02-10 13:29:31 -0500

    completed assertArraysEqual.js file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,11 +1,13 @@
-const eqArrays = function(actual, expected) {
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArrays");
+
+// const eqArrays = function(actual, expected) {
+//   for (let i = 0; i < actual.length; i++) {
+//     if (actual[i] !== expected[i]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
 
 const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
