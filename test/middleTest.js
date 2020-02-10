ee65f449aabdb54d11bd4400e4da2ce2409cd0d8
David <david.he07@yahoo.ca>
2020-02-10 14:39:30 -0500

    completed middleTest.js file

---
+++ test/middleTest.js
@@ -0,0 +1,18 @@
+// const assertArraysEqual = require("../assertArraysEqual");
+const middle = require("../middle");
+const assert = require("chai").assert;
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
+
+// middle([1]); // => []
+// middle([1, 2]); // => []
+
+// middle([1, 2, 3]); // => [2]
+// middle([1, 2, 3, 4, 5]); // => [3]
+
+// middle([1, 2, 3, 4]); // => [2, 3]
+// middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
