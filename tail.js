f37d374b138efdb799b3d9e75fcdc8f46d3cf758
David <david.he07@yahoo.ca>
2020-02-10 13:10:53 -0500

    completed tail.js file

--- tail.js
+++ tail.js
@@ -1,18 +1,20 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:, ✅ ✅ ✅  ${actual} === ${expected}`);
-  }
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed:, ✅ ✅ ✅  ${actual} === ${expected}`);
+//   }
 
-  if (actual !== expected) {
-    console.log(`Assertion Failed:, 🛑 🛑 🛑  ${actual} !== ${expected}`);
-  }
-};
+//   if (actual !== expected) {
+//     console.log(`Assertion Failed:, 🛑 🛑 🛑  ${actual} !== ${expected}`);
+//   }
+// };
 
 const tail = function(actual) {
   let answer = actual.slice(1);
   return answer;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3);
+
+module.exports = tail;
