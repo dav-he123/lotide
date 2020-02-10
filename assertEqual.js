e56a4f094696d01bcd07d62154147b889d626287
David <david.he07@yahoo.ca>
2020-02-10 13:03:49 -0500

    completed assertEqual.js file

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,16 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed:", `✅ ✅ ✅  ${actual} === ${expected}`);
+    console.log(`Assertion Passed:, ✅ ✅ ✅  ${actual} === ${expected}`);
   }
 
   if (actual !== expected) {
-    console.log("Assertion Failed:", `🛑 🛑 🛑  ${actual} !== ${expected}`);
+    console.log(`Assertion Failed:, 🛑 🛑 🛑  ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+module.exports = assertEqual;
